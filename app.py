62bef7cad078030539e456ef6d02d66d982e2f96
tanvirahmed007 <tanvir15-9909@diu.edu.bd>
2022-04-07 10:00:06 +0100

    variable types

--- app.py
+++ app.py
@@ -1,5 +1,55 @@
 
-x =2
-y=2
-z=x+y
-print("{}".format(z))
+
+#data type
+
+# int
+
+num = 10
+print(f'int type: {num}')
+
+
+# float
+
+num2 = 10.5
+print(f'float type: {num2}')
+
+
+# basic numeric operations
+
+x = 10
+y = 5
+
+print(f'x + y = {x + y}')
+print(f'x - y = {x - y}')
+print(f'x * y = {x * y}')
+print(f'x / y = {x / y}')
+
+# power
+print(f'x ** y = {x ** y}')
+
+# remain
+print(f'x % y = {x % y}')
+
+
+# string
+fname = 'Tanvir'
+lname = 'Ahmed'
+
+print(f'{fname} {lname}')
+
+
+# under the hood unicode
+
+s1 = chr(97)
+s2 = chr(98)
+s3 = chr(99)
+print(s1+s2+s3)
+
+
+# escape character
+
+# \n new line
+# \t tab
+# \b backspace
+# \r new line
+# \ appostrophe
