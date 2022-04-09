3726fd78964e78d146d7581755b0c28645048598
tanvirahmed007 <tanvir15-9909@diu.edu.bd>
2022-04-09 12:36:50 +0600

    For loop, List, Tuples, Dictionary, Function, Modules

--- app.py
+++ app.py
@@ -216,20 +216,223 @@
 
 # car game ======================================================
 
-print('Welcome to the car game\n')
-print('You have to enter the command to the car.\n start - car will start\n stop - car will stop\n quit - game will end')
-carStatus = input('')
+# print('Welcome to the car game\n')
+# print('You have to enter the command to the car.\n start - car will start\n stop - car will stop\n quit - game will end')
+# carStatus = input('')
 
-while carStatus != 'quit':
-    if carStatus == 'start':
-        print('Car started')
-    elif carStatus == 'stop':
-        print('Car stopped')
-    elif carStatus == 'quit':
-        print('Game ended')
-        break
-    else:
-        print('I don\'t understand that command')
+# while carStatus != 'quit':
+#     if carStatus == 'start':
+#         print('Car started')
+#     elif carStatus == 'stop':
+#         print('Car stopped')
+#     elif carStatus == 'quit':
+#         print('Game ended')
+#         break
+#     else:
+#         print('I don\'t understand that command')
 
-    carStatus = input('')
+#     carStatus = input('')
+
+
+# for loop ======================================================
+# for item in ['Python', 'Java', 'C++']:
+#     print(item)
+
+
+# for item in range(10):
+#     print(item)
+
+# for item in range(5, 10):
+#     print(item)
+
+# for item in range(0, 10, 2):
+#     print(item)
+
+
+# prices = [10, 20, 30]
+# total = 0
+# for price in prices:
+#     total += price
+# print(f'Total: {total}')
+
+# nested loop==============
+
+# for x in range(4):
+#     for y in range(3):
+#         print(f'({x}, {y})')
+
+
+# numbers = [2, 2, 2, 2, 5]
+
+# for item in numbers:
+#     output = ''
+#     for num in range(item):
+#         output += 'x'
+#     print(output)
+
+
+numbers = [12, 46, 33, 23, 2]
+
+# for item in numbers:
+#     output = max(numbers)
+# print(output)
+
+# # other way
+
+# maxNumber = numbers[0]
+# for item in numbers:
+#     if item > maxNumber:
+#         maxNumber = item
+# print(maxNumber)
+
+
+# 2D list===========================================================
+
+# numbers = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# print(numbers)
+    
+# list methods ======================================================
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# numbers.append(11) # add item to the end of the list
+# numbers.insert(0, 0) # insert item at the specified index
+# numbers.remove(11) # remove item from the list
+# numbers.pop() # remove last item from the list
+# numbers.clear() # remove all items from the list
+# numbers.index(5) # return the index of the item
+
+# print(numbers.count(5)) # return the number of times the item appears in the list
+# print(numbers.sort()) # sort the list
+# print(numbers.reverse()) # reverse the list
+# print(numbers)
+
+# numbers2 = numbers.copy() # copy the list
+# print(numbers2)
+
+
+# numbers = [1, 2, 2, 6, 3, 4, 5, 6, 7, 8, 9, 10]
+# unique = []
+# for item in numbers:
+#     if item not in unique:
+#         unique.append(item)
+# print(unique)
+
+
+# tuples ============================================================
+
+# numbers = (1, 2, 3, 4)
+# x, y, z, t = numbers
+# print('x = {}, y = {}, z = {}, t = {}'.format(x, y, z, t))
+
+
+# dictionary ============================================================
+
+# customer = {
+#     'name': 'John Smith',
+#     'age': 30,
+#     'is_verified': True
+# }
+
+# print(customer['name'])
+# print(customer.get('birthdate', 'Jan 1 1980'))
+# print(customer['age'])
+# print(customer['is_verified'])
+# print(customer)
+
+
+
+# emoji converter ===========================
+
+# message = input('> ')
+# words = message.split(' ')
+# emojis = {
+#     ':)': '😊',
+#     ':(': '😞'
+# }
+# output = ''
+# for word in words:
+#     output += emojis.get(word, word) + ' '
+    
+# print(output)
+
+
+# function ============================================================
+
+# def index():
+#     print('Welcome to the homepage')
+
+
+# index()
+
+# parameters =============
+# def greet(name, greeting):
+#     print(f'{greeting} {name}')
+
+
+# greet("Tanvir","Welcome") # parameters
+
+
+
+# classes ====================================================
+# class Point:
+#     def move(self):
+#         print('move')
+
+#     def draw(self):
+#         print('draw')
+
+
+# point1 = Point()
+# point1.x = 10
+# point1.y = 20
+# print(point1.x)
+# point1.draw()
+
+
+
+# constructor ===================
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def move(self):
+#         print('move')
+
+#     def draw(self):
+#         print('draw')
+
+
+# point1 = Point(101, 102)
+
+# print(point1.y)
+# point1.draw()
+
+
+
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def talk(self):
+#         print(f'Hi, I am {self.name}')
+
+
+# person1 = Person('Tanvir')
+# person1.talk()
+
+
+
+# modules ========================================================
+from convert import kgToLbs
+
+result = kgToLbs(100)
+
+print(result)
 
---
+++ convert.py
@@ -0,0 +1,6 @@
+def lbsToKG(weight):
+    return weight * 0.45
+
+
+def kgToLbs(weight):
+    return weight * 2.20
